af0a8dac29101837396b64b836f8bfa0e80f6696
걷는사람 정정훈 <92363576+walkerJung@users.noreply.github.com>
2021-12-21 22:10:05 +0900

    2021-12-21 22:10

---
+++ type_example.js
@@ -0,0 +1,16 @@
+var 이름 = "JungHoon";
+var 나이 = 32;
+var 결혼했니 = false;
+var 널 = null;
+var 언디파인드 = undefined;
+var 배열 = ["Jung", "Ahn", "Oh"];
+var 객체 = {
+    member1: "Jung",
+    member2: "Ahn",
+};
+// 강의 마무리문제
+var project = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
---
+++ type_example.ts
@@ -0,0 +1,17 @@
+let 이름: string = "JungHoon";
+let 나이: number = 32;
+let 결혼했니: boolean = false;
+let 널: null = null;
+let 언디파인드: undefined = undefined;
+let 배열: string[] = ["Jung", "Ahn", "Oh"];
+let 객체: { member1: string; member2: string } = {
+  member1: "Jung",
+  member2: "Ahn",
+};
+
+// 강의 마무리문제
+let project: { member: string[]; days: number; started: boolean } = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
